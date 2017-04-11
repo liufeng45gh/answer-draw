649b069992cfcdc3ec2a8e81e4ef9dd266e81cef
刘峰选 <liufengxuan@9hcom.com>
2017-04-11 14:52:47 +0800

    test

--- src/main/resources/static/web/js/get-score.js
+++ src/main/resources/static/web/js/get-score.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $(".a-text").get(0).addEventListener('touchend', function(e) {
-        window.location.href = "/get-reward";
+        window.location.href = "http://m.lyancoffee.com/wechat/lucky/929531/0";
     });
 });
