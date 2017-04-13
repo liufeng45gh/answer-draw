1e857a4179fe0ac3c323e76808ba49551db67b04
刘峰选 <liufengxuan@9hcom.com>
2017-04-13 17:16:21 +0800

    test

--- src/main/java/com/lucifer/utils/Constant.java
+++ src/main/java/com/lucifer/utils/Constant.java
@@ -7,5 +7,8 @@ import java.util.Date;
 public class Constant {
 	
 	public static final String KEY_COFFEE_REWARD_COUNT = "ANSWER-DRAW:KEY-COFFEE-REWARD-COUNT";
+
+
+	public static final String KEY_WX_JSAPI_TICKET = "ANSWER-DRAW:KEY-WX-JSAPI-TICKET";
 	
 }
