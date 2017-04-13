4ccbea7700f4c7699a3b7ea7e96cddc4d9d6f0d8
刘峰选 <liufengxuan@9hcom.com>
2017-04-13 18:16:29 +0800

    bugfix

--- src/main/resources/static/web/js/wx-share.js
+++ src/main/resources/static/web/js/wx-share.js
@@ -78,7 +78,14 @@ $(document).ready(function() {
                     imgUrl: shareImgUrl // 分享图标
                     });
 
-                });  
+                });
+
+                wx.error(function(res){
+
+                    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+                   alert("微信设置错误");
+                   alert(res);
+                });
 
         });
 
