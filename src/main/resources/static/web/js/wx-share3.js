6aefc10b343a1f5196fbcdd3369ad05aabfed881
刘峰选 <liufengxuan@9hcom.com>
2017-04-14 15:06:38 +0800

    test

--- src/main/resources/static/web/js/wx-share3.js
+++ src/main/resources/static/web/js/wx-share3.js
@@ -20,7 +20,7 @@
 
                 wx.ready(function(){
                     wx.onMenuShareTimeline({
-                        title: '测一测你的投行人生是什么段位',
+                        title: '测一测，你的投行人生是什么段位？',
                         link: surl,
                         imgUrl: iurl,
                         success: function () {
@@ -44,7 +44,7 @@
                         }
                     });
                     wx.onMenuShareAppMessage({
-                        title: '测一测你的投行人生是什么段位',
+                        title: '测一测，你的投行人生是什么段位？',
                         desc: '「华泰联合证券微信公众号启动1周年」在线互动',
                         link: surl,
                         imgUrl: iurl,
